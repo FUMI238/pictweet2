577ba16abb266cbd77ac5c43154beeea8d732413
鈴木文也 <suzukifumiya@FS-MacBook-Pro.local>
2018-03-15 19:23:38 +0900

    complete add_user_info_to_tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,7 +10,7 @@ class TweetsController < ApplicationController
   end
 
   def create
-    Tweet.create(tweet_params)
+    Tweet.create(name: tweet_params[:name],image:tweet_params[:image],text: tweet_params[:text], user_id: current_user.id)
   end
 
   private
@@ -19,6 +19,6 @@ class TweetsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_singed_in?
+    redirect_to action: :index unless user_signed_in?
   end
 end
